55b30377342dc8f747f5e072ea98d0102fb75f3f
Joshua Pereira <71769388+Josh7511@users.noreply.github.com>
2025-05-26 12:16:38 -0400

    created user serializer

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class User(models.model):
+class User(models.Model):
     username = models.CharField(max_length=30, unique=True)
     email = models.EmailField(max_length=254, unique=True)
     password = models.CharField(max_length=128)
