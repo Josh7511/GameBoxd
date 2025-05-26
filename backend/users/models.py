57694bee661a37259f6c57494b7e87af140063b8
Joshua Pereira <71769388+Josh7511@users.noreply.github.com>
2025-05-26 12:05:38 -0400

    created user app and set up user model

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class User(models.model):
+    username = models.CharField(max_length=30, unique=True)
+    email = models.EmailField(max_length=254, unique=True)
+    password = models.CharField(max_length=128)
+    first_name = models.CharField(max_length=30, blank=True)
+    last_name = models.CharField(max_length=30, blank=True)
+    bio = models.TextField(blank=True)
+    avatar = models.ImageField(upload_to='avatars/', blank=True, null=True) 
