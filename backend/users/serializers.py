736e82c63739d453cfac0fcf74e27bf234e9ccd1
Joshua Pereira <71769388+Josh7511@users.noreply.github.com>
2025-05-27 11:23:39 -0400

    create the create function to implement hashing of the password

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -13,4 +13,13 @@ class UserCreateSerializer(serializers.ModelSerializer):
         fields = ['username', 'email', 'password']
         extra_kwargs = {'password': {'write_only': True}}
 
-        
+
+
+def create(self, validated_data):
+        user = User(
+            username=validated_data['username'],
+            email=validated_data['email']
+        )
+        user.set_password(validated_data['password'])  # Hash the password
+        user.save()
+        return user
