9c39585062ef1c413baf5cc209eb7dd3e1c4d44f
Joshua Pereira <71769388+Josh7511@users.noreply.github.com>
2025-05-27 11:35:00 -0400

    created the url path for the register user api request

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from users.views import register_user
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -25,6 +26,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
      path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/register/', YourRegistrationView.as_view())
+    path('api/register/', register_user),
 ]
 
