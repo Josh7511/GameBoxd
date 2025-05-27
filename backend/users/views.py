e75667fb0ff859ea4cb5c16f31bca49a6b4cd5c9
Joshua Pereira <71769388+Josh7511@users.noreply.github.com>
2025-05-27 11:33:41 -0400

    created register user api view

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from users.serializers import UserCreateSerializer
 
 # Create your views here.
+@api_view(['POST'])
+
+def register_user(request):
+    # This is handling user registration
+    if request.method == 'POST':
+        serializer = UserCreateSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            return Response({"message": "User created successfully", "user_id": user.id}, status=201)
+        return Response(serializer.errors, status=400)
+        
