5d2a7d6db299756b0f0421e5ecb354491ad7fdf7
Joshua Pereira <71769388+Josh7511@users.noreply.github.com>
2025-05-30 15:02:23 -0400

    url path added for api request

--- backend/games/urls.py
+++ backend/games/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import game_list
+from .views import game_list, game_log
 
 urlpatterns = [
     path('games/', game_list, name='game-list'),
+    path('game-log/',game_log, name='game-log'),
 ]
