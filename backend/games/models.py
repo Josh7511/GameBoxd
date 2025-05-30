c8e8e133b39136ec119b61859437e8f0f9a00b6b
Joshua Pereira <71769388+Josh7511@users.noreply.github.com>
2025-05-30 11:34:21 -0400

    created models for game

--- backend/games/models.py
+++ backend/games/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+class Game(models.Model):
+    igdb_id = models.IntegerField(unique=True)
+    title = models.CharField(max_length=255)
+    def __str__(self):
+        return self.title
+
+class GameLog(models.Model):
+    STATUS_CHOICES = [
+        ('playlist', 'Playlist'),
+        ('completed', 'Completed'),
+        ('inprogress', 'In Progress'),
+    ]
+
+    user = models.ForeignKey('users.User', on_delete=models.CASCADE)
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+    status = models.CharField(max_length=20, choices = STATUS_CHOICES)  # playlist, completed, inprogress
+    rating = models.IntegerField(null=True, blank=True)  # 1-5 rating
+    date_played = models.DateTimeField(auto_now_add=True)
+    review = models.TextField(blank=True, null=True)
