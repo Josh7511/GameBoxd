a8e0fe98c1b2bb82f138e4e11ca4764f9e6c70e0
Joshua Pereira <71769388+Josh7511@users.noreply.github.com>
2025-05-30 12:10:56 -0400

    created the view and url for the game

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from users.views import register_user
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
@@ -27,5 +27,6 @@ urlpatterns = [
      path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/register/', register_user),
+    path('api/', include('games.urls')),
 ]
 
--- backend/games/views.py
+++ backend/games/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Game
+from games.serializers import GameSerializer, GameLogSerializer
 
-# Create your views here.
+@api_view(['GET', 'POST'])
+
+def game_list(request):
+    #pulling from my local game table
+    if request.method == "GET":
+        games = Game.objects.all()
+        serializer = GameSerializer(games, many=True)
+        return Response(serializer.data)
+        
