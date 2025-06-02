aa54d3362bf436aa4759b087a24877f8c4888c3b
Joshua Pereira <71769388+Josh7511@users.noreply.github.com>
2025-06-02 14:14:18 -0400

    configured search api endpoint

--- backend/games/urls.py
+++ backend/games/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import game_list, game_log
+from .views import game_list, game_log, search_igdb
 
 urlpatterns = [
     path('games/', game_list, name='game-list'),
     path('games-log/',game_log, name='game-log'),
+    path('search-igdb/', search_igdb, name='search-igdb'),
 ]
