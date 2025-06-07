fdabc3394f681b219b80351974a041e7cf7f23c5
Joshua Pereira <71769388+Josh7511@users.noreply.github.com>
2025-06-07 14:44:41 -0400

    frontend is now pulling reviews

--- frontend/src/components/GameReviews.js
+++ frontend/src/components/GameReviews.js
@@ -1,22 +1,31 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 
 function GameReviews() {
+  const { id } = useParams();
+  const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    fetch(`http://localhost:8000/api/game-log-by-id/?query=${id}`)
+      .then((response) => response.json())
+      .then((data) => setResults(data))
+      .catch((error) => console.error('Fetch Error', error));
+  }, [id]);
 
 
-    fetch('http://localhost:8000/api/games-log/', {
-        headers: {
-          'Authorization': `Bearer ${localStorage.getItem('access_token')}`
-        }
-      })
-      .then(res => res.json())
-      .then(data => console.log(data));
-      
 return(
-    <div className="game-reviews">
-        <h2>Game Reviews</h2>
-        <p>Check the console for fetched reviews.</p>
-    </div>
+  <div className="game-reviews">
+  {results.length === 0 ? (
+      <p>No summary available for this game.</p>
+  ) : (
+      results.map((log) => (
+          <div key={log.user}>
+              <p>{log.rating}</p>
+              <p>{log.review}</p>
+          </div>
+      ))
+  )}
+</div>
 );
 }
 
