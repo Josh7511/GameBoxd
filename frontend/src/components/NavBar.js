96e5a809aaa55bc9bedd347c94050497bf114a11
Joshua Pereira <71769388+Josh7511@users.noreply.github.com>
2025-06-17 22:39:59 -0400

    backend pull genre

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useNavigate, Link } from 'react-router-dom';
+import { useNavigate, Link} from 'react-router-dom';
 import './NavBar.css';
 import controller from '../assets/images/controller.png';
 
