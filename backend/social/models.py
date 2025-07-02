9a6cb02e8809dc527c7d31652195d6a0fdb95f6c
Joshua Pereira <71769388+Josh7511@users.noreply.github.com>
2025-07-02 13:27:42 -0400

    helper functions

--- backend/social/models.py
+++ backend/social/models.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.db import models
+from django.contrib.auth import get_user_model
 
 class Follow(models.Model):
     follower = models.ForeignKey(
@@ -20,3 +21,21 @@ class Follow(models.Model):
 
     def __str__(self):
         return f"{self.follower} → {self.followee}"
+
+
+User = get_user_model()
+
+def follow(self, other_user):
+    if self == other_user:
+        return
+    Follow.objects.get_or_create(follower=self, followee=other_user)
+
+def unfollow(self, other_user):
+    Follow.objects.filter(follower=self, followee=other_user).delete()
+
+def is_following(self, other_user):
+    return Follow.objects.filter(follower=self, followee=other_user).exists()
+
+User.add_to_class("follow", follow)
+User.add_to_class("unfollow", unfollow)
+User.add_to_class("is_following", is_following)
